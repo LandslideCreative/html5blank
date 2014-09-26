4e92bc8bf1f395537f0fc07b01601d4538b18b2b
Josh Jenkins <jsjenkins@gmail.com>
2014-09-25 23:09:18 -0500

    Move modernizr, add fastclick

--- header.php
+++ header.php
@@ -12,13 +12,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
         </script>
 
 	</head>
