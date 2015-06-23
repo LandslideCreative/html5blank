8238811c35a31be6ebda2cc5155733b60e6c7a65
Josh Jenkins <jsjenkins@gmail.com>
2015-06-22 23:37:48 -0500

    Remove extra sidebar

--- inc/sidebars.php
+++ inc/sidebars.php
@@ -5,20 +5,9 @@ if (function_exists('register_sidebar'))
 {
     // Define Sidebar Widget Area 1
     register_sidebar(array(
-        'name' => __('Widget Area 1', 'html5blank'),
-        'description' => __('Description for this widget-area...', 'html5blank'),
-        'id' => 'widget-area-1',
-        'before_widget' => '<div id="%1$s" class="%2$s">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>'
-    ));
-
-    // Define Sidebar Widget Area 2
-    register_sidebar(array(
-        'name' => __('Widget Area 2', 'html5blank'),
-        'description' => __('Description for this widget-area...', 'html5blank'),
-        'id' => 'widget-area-2',
+        'name' => __('Main Sidebar', 'html5blank'),
+        'description' => __('Main sidebar for the site.', 'html5blank'),
+        'id' => 'main-sidebar',
         'before_widget' => '<div id="%1$s" class="%2$s">',
         'after_widget' => '</div>',
         'before_title' => '<h3>',
